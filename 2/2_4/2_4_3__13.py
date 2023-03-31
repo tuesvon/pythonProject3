a5446e2c1ace5a64d772b60b1edcc14d5419c846
Dimitriy Kanaev <72267931+tuesvon@users.noreply.github.com>
2023-03-31 18:05:15 +0300

    Всё по 2.4

---
+++ 2/2_4/2_4_3__13.py
@@ -0,0 +1,81 @@
+# 2.4.3
+# x = 3
+# y = 4
+# z = x + y #7 = 3 + 4
+# z = z + 1 #8 = 7 + 1
+# x = y #x = 4
+# y = 5
+# x = z + y + 7 #x = 8 + 5 + 7
+# print(x)
+
+# 2.4.4
+# a = 4
+# print(a, 'a')
+
+# 2.4.5 / Три последовательных числа
+# num1 = int(input())
+# num2 = num1 + 1
+# num3 = num2 + 1
+# print(num1)
+# print(num2)
+# print(num3)
+
+# 2.4.6 / Сумма трёх чисел
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# print(a + b + c)
+
+# 2.4.7 / Куб
+# a = int(input())
+# formulaV = a**3
+# formulaS = 6 * a**2
+# print('Объем =', formulaV)
+# print('Площадь полной поверхности =', formulaS)
+
+# 2.4.8 / Значение функции
+# a = int(input())
+# b = int(input())
+# func = 3 * (a + b)**3 + 275 * b**2 - 127 * a - 41
+# print(func)
+
+# 2.4.9 / Следующее и предыдущее
+# a = int(input())
+# b = a + 1
+# c = a - 1
+# print('Следующее за числом', a, 'число:', b)
+# print('Для числа', a, 'предыдущее число:', c)
+
+# 2.4.10 / Стоимость покупки
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# d = int(input())
+# price = 3 * (a + b + c + d)
+# print(price)
+
+# 2.4.11 / Арифметические операции
+# a = int(input())
+# b = int(input())
+# c = a + b
+# d = a - b
+# e = a * b
+# print(a, '+', b, '=', c)
+# print(a, '-', b, '=', d)
+# print(a, '*', b, '=', e)
+
+# способ из комментария
+# a = int(input())
+# b = int(input())
+# print(f"{a} + {b} = {a + b}")
+
+# 2.4.12 / Арифметическая прогрессия
+# a = int(input())
+# d = int(input())
+# n = int(input())
+# an = a + d * (n - 1)
+# print(an)
+
+# 2.4.13 / Разделяй и властвуй
+# x = int(input())
+# print(x, 2 * x, 3 * x, 4 * x, 5 * x, sep='---')
