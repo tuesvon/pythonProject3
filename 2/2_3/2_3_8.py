847e4f545bf098e1cf607be95d6950a5d53f667d
Dimitriy Kanaev <72267931+tuesvon@users.noreply.github.com>
2023-03-31 15:41:32 +0300

    Изменения в директории: создание директорий для подмодулей, чтобы не плодить одно большое дерево

---
+++ 2/2_3/2_3_8.py
@@ -0,0 +1,21 @@
+#variable_name = input()
+#print('Вы ввели текст:', variable_name)
+
+#name, surname = 'Dima', 'Kanaev'
+#print('Name:', name, 'Surname:', surname)
+
+#name, surname = input(), input()
+#print("Name:", name, "Surname:", surname)
+
+#language = 'Russian'
+#language = 'English'
+#print(language)
+
+# print('Java')
+# print('Ruby')
+# print('Scala')
+print('Python', end='+')  # print('C++')
+# print('GO')
+print('C#', end='=')  # print('C')
+print('awesome')
+# finish
