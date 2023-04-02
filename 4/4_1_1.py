329781b7127509b94bff11111dc62e7a52a7fe70
Dimitriy Kanaev <72267931+tuesvon@users.noreply.github.com>
2023-04-02 14:05:50 +0300

    - В readme описал поподробнее, что означают директории в данном проекте - Создан репозиторий к урокам в 4 модуле (Условный оператор)

---
+++ 4/4_1_1.py
@@ -0,0 +1,19 @@
+# условный оператор
+# answer = input('Какой язык программирования мы изучаем?')
+# if answer == 'Python':
+#    print('Верно! Мы ботаем Python =)')
+#    print('Python - отличный язык!')
+# else:
+#    print('Не совсем так!')
+
+# num1 = int(input())
+# num2 = int(input())
+
+# if num1 < num2:
+#    print(num1, 'меньше чем', num2)
+# if num1 > num2:
+#    print(num1, 'больше чем', num2)
+# if num1 == num2:                      # используем двойное равенство
+#    print(num1, 'равно', num2)
+# if num1 != num2:
+#    print(num1, 'не равно', num2)
